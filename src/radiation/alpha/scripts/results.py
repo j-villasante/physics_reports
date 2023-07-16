284620e2b5599e0a74d14d14438b4837d95ed48f
Josue Villasante <josue.villasante@gmail.com>
2023-07-16 17:51:40 -0500

    feat: finished radiation reports

--- src/radiation/alpha/scripts/results.py
+++ src/radiation/alpha/scripts/results.py
@@ -25,6 +25,9 @@ def linear(x, a, b):
 def inverse_linear(y, a, b):
     return round((y - b) / a)
 
+def cuad(x, a, b, c):
+    return a * x**2 + b * x + c
+
 def normal(x, n, sigma, mu):
     return n * np.exp(-(((x - mu) / sigma) ** 2) / 2) / (sigma * sqrt(2 * pi))
 
@@ -44,6 +47,13 @@ ax.set_xlabel("Canal")
 ax.set_ylabel("Energía (keV)")
 plt.savefig(f"{img_dir}/calibration.pdf", bbox_inches="tight")
 
+# Calibration for teacher's experiment
+popt2 = curve_fit(
+    linear,
+    np.array([538.79, 576.02, 613.33]),
+    np.array([4825, 5156, 5486]),
+)[0]
+
 f = open(current_dir + "/spec_params.json", encoding="utf8")
 spec_params = load(f)
 
@@ -55,14 +65,22 @@ table = {
     "net_area": [],
 }
 
+range_energies = []
+
 for image in spec_params:
     # Plot spectrum
     fig, ax = plt.subplots()
 
     if "file" in image:
-        x, y = np.genfromtxt(f"{data_dir}/{image['file']}", delimiter=",").T
+        data = np.genfromtxt(f"{data_dir}/{image['file']}", delimiter=",").T
+        x = np.arange(0, 1024)
+        y = data if "only_data" in image else data[1]
+
         if image["use_calibration"]:
-            x = linear(x, *popt)
+            if image["use_calibration"] == "2":
+                x = linear(x, *popt2)
+            else:
+                x = linear(x, *popt)
 
             for peak in (image["peaks"] if "peaks" in image else []):
                 half_point = peak["start"] + ((peak["end"] - peak["start"]) / 2)
@@ -80,7 +98,10 @@ for image in spec_params:
                 # ax.plot(np.array([mu - delta, mu - delta]), np.array([0, 100]))
                 # ax.plot(np.array([mu + delta, mu + delta]), np.array([0, 100]))
                 
-                area = np.sum(y[inverse_linear(mu - delta, *popt) : inverse_linear(mu + delta, *popt)])
+                if "manual_area" in image and image["manual_area"] is True:
+                    area = np.sum(_y)
+                else:
+                    area = np.sum(y[inverse_linear(mu - delta, *popt) : inverse_linear(mu + delta, *popt)])
                 if "area" in peak and peak["area"]:
                     areas.append(area)
                     ax.annotate(f"$A = {area:.3}$", (mu + peak["dx"], peak["y"]))
@@ -92,6 +113,8 @@ for image in spec_params:
                 table["energy"].append(mu)
                 table["fwhm"].append(delta)
                 table["net_area"].append(f"a{area:.3g}")
+                if image["name"].startswith("triple"):
+                    range_energies.append(mu)
 
         ax.plot(x, y, linewidth=0.75)
     else:
@@ -110,4 +133,24 @@ for image in spec_params:
     plt.savefig(f"{img_dir}/{image['name']}.pdf", bbox_inches="tight")
 
 
+fig, ax = plt.subplots()
+x, y = np.genfromtxt(f"{data_dir}/srim", delimiter=",").T
+(popt, pcov) = curve_fit(cuad, x, y)
+_x = np.linspace(np.amin(x), np.amax(x))
+ax.plot(
+    _x,
+    cuad(_x, *popt),
+    label=f"$y = {popt[0]:.04g}x^2+{popt[1]:.04g}x+{popt[2]:.04g}$",
+)
+ax.plot(x, y, "o")
+ax.set_xlabel("Energía")
+ax.set_ylabel("Alcance")
+ax.legend()
+plt.savefig(f"{img_dir}/srim.pdf", bbox_inches="tight")
+
+print(tabulate({
+    "energy": range_energies,
+    "range": map(lambda x: cuad(x, *popt), range_energies)
+}, headers="keys"))
+print("\n")
 print(tabulate(table, headers="keys"))
--- src/radiation/gamma/scripts/results.py
+++ src/radiation/gamma/scripts/results.py
@@ -61,7 +61,7 @@ for image in spec_params:
     fig, ax = plt.subplots()
 
     if "file" in image:
-        x, y = np.genfromtxt(f"{data_dir}/{image['file']}", delimiter="\t").T
+        x, y = np.genfromtxt(f"{data_dir}/{image['file']}", delimiter=",").T
         if image["use_calibration"]:
             x = linear(x, *popt)
 
@@ -76,9 +76,12 @@ for image in spec_params:
                 #     normal(_x, n, sigma, mu),
                 #     label=f"$\mu={round(mu)}, \sigma={round(sigma)}$",
                 # )
-                # ax.plot(np.array([mu - delta, mu - delta]), np.array([0, 5000]))
-                # ax.plot(np.array([mu + delta, mu + delta]), np.array([0, 5000]))
-                area = np.sum(y[inverse_linear(mu - delta, *popt) : inverse_linear(mu + delta, *popt)])
+                # ax.plot(np.array([linear(peak["start"], *popt), linear(peak["start"], *popt)]), np.array([0, 5000]))
+                # ax.plot(np.array([linear(peak["end"], *popt), linear(peak["end"], *popt)]), np.array([0, 5000]))
+                if "manual_area" in image and image["manual_area"] is True:
+                    area = np.sum(_y) - ((peak["end"] - peak["start"]) * (y[peak["start"]] + y[peak["end"]]) / 2)
+                else:
+                    area = np.sum(y[inverse_linear(mu - delta, *popt) : inverse_linear(mu + delta, *popt)])
                 if "area" in peak and peak["area"]:
                     areas.append(area)
                     ax.annotate(f"$A = {area:.3}$", (mu + peak["dx"], peak["y"]))
@@ -95,7 +98,7 @@ for image in spec_params:
         ax.plot(x, y, linewidth=0.75)
     else:
         for spectrum in image["files"]:
-            x, y = np.genfromtxt(f"{data_dir}/{spectrum['filename']}", delimiter="\t").T
+            x, y = np.genfromtxt(f"{data_dir}/{spectrum['filename']}", delimiter=",").T
             ax.plot(x, y, label=spectrum["label"], linewidth=0.5)
     
     # Add labels
@@ -114,18 +117,22 @@ x = 11.34 * x / 10
 y = np.log(np.array(areas))
 
 fig, ax = plt.subplots()
-popt = curve_fit(linear, x, y)[0]
+(popt, pcov) = curve_fit(linear, x, y)
 _x = np.linspace(np.amin(x), np.amax(x))
 ax.plot(
     _x,
     linear(_x, *popt),
-    label=f"$y = {popt[0]:.03g}x+{popt[1]:.03g}$",
+    label=f"$y = {popt[0]:.04g}x+{popt[1]:.04g}$",
 )
 ax.plot(x, y, "o")
-ax.set_xlabel("$\\rho x$ (g/mm²)")
+ax.set_xlabel("$\\rho x$ (g/cm²)")
 ax.set_ylabel("$\\ln(I_\\gamma)$")
 ax.legend()
 plt.savefig(f"{img_dir}/thickness_vs_area.pdf", bbox_inches="tight")
+
+perr = np.sqrt(np.diag(pcov))
+print(popt)
+print(f"Attenuation error: {perr}")
 
 table["ep"] = x
 print(tabulate(table, headers="keys"))
